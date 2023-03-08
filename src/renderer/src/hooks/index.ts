a9574a9555b553ef98eb83279d9c0e77b33d6557
김선민 <tjsals116@gmail.com>
2023-03-08 21:07:52 +0900

    finish button action without "=" button action

--- src/renderer/src/hooks/index.ts
+++ src/renderer/src/hooks/index.ts
@@ -1,8 +1,22 @@
 import { useCallback } from 'react'
 
-const inputs: ButtonType[] = []
-const outputs: ButtonType[] = []
+// const inputs: (ButtonType | number)[] = []
+const outputs: (ButtonType | number)[] = []
 const operators: ButtonType[] = []
+
+const calculate = (data: string): string => {
+  for (const token of data.split(/(\+|-|\*|\/|%)/) as ButtonType[]) {
+    const isNaN = Number.isNaN(Number(token))
+    if (isNaN) {
+      operators.push(token)
+    } else {
+      outputs.push(token)
+    }
+  }
+  console.log('outputs:', outputs)
+  console.log('operators:', operators)
+  return '110'
+}
 
 /**
  * * button의 입력을 받아서 액션을 실행하는 함숫
@@ -14,8 +28,12 @@ export const useClickButton = (): ((buttonType: ButtonType, data: string) => str
 
     switch (buttonType) {
       case '=':
-        /** TODO calculate */
-        break
+        // 마지막 글자가 /(\+|\\-|\*|\/|%|\.)$/g 일 경우
+        if (data.match(/(\+|-|\*|\/|%|\.)$/g)?.[0]) {
+          return data
+        }
+
+        return calculate(data)
       case 'C':
         return ''
       case '%':
@@ -23,20 +41,28 @@ export const useClickButton = (): ((buttonType: ButtonType, data: string) => str
       case '+':
       case '-':
       case '/':
-        if (['%', '*', '+', '-', '/'].includes(data?.at(-1) ?? '')) {
-          return data
+        if (data.match(/(\+|-|\*|\/|%)$/g)?.[0]) {
+          return data.replace(/(\+|-|\*|\/|%)$/g, buttonType)
         }
+
         if (data.length === 0) {
           return data
         }
         return addedValue
       case '0':
-        return '0' === data?.at(-1) ? data : addedValue
+        // string의 맨 마지막에 있는 숫자들이 이루는 수를 구하는 코드, 소수점 포함
+        // eslint-disable-next-line no-case-declarations
+        const lastNumber = data.match(/(\d+\.?\d*)$/)?.[0] ?? ''
+
+        if (lastNumber.at(0) === '0' && lastNumber.at(1) !== '.') {
+          return data
+        }
+
+        return addedValue
       case '.':
         return data.includes('.') ? data : addedValue
       default:
         return addedValue
     }
-    return ''
   }, [])
 }
